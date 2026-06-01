a7bd1baa1c5fcfe7422e5b5be3d9f8337ed9046c
Waleed Amgad <waleedbookey@gmail.com>
2026-06-01 20:55:21 +0300

    attempt 20

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,15 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.node, // Since you are using a server.js, you likely need node globals, not just browser
+        ...globals.browser
+      }
+    }
+  }
+];
